20e75ecc826e302fb4a5add3a812bc1f8cc099fa
issuebombom <issuebombom@gmail.com>
2024-02-14 14:14:32 +0900

    first commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "# login-signup-best-practice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:issuebombom/login-signup-best-practice.git
+git push -u origin main
