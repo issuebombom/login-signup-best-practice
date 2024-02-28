45a2b027b55de48e20bb9a99b939e76370eb9ebc
issuebombom <79882498+issuebombom@users.noreply.github.com>
2024-02-28 14:30:22 +0900

    fix: genId func.

--- src/exception/BusinessException.ts
+++ src/exception/BusinessException.ts
@@ -19,8 +19,8 @@ export class BusinessException extends Error {
 
   private static genId(length = 12): string {
     const p = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    return [...Array(length)].reduce((a) => {
+    return [...Array(length)].reduce((a) => 
       a + p[Math.floor(Math.random() * p.length)], '';
-    });
+    );
   }
 }
